ba7bcee72641b898a7a8a39415c7612c3dbdd205
Aryan Kumar <aryankumar79922@gmail.com>
2024-10-06 14:50:50 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# lesson-planner
+ Lesson Planner Api for the NASA Space App Challenge on SDGs on Classroom
---
+++ src/test/java/com/nasa_space_app/sdgs_web_app/lesson_planner/LessonPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nasa_space_app.sdgs_web_app.lesson_planner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LessonPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
